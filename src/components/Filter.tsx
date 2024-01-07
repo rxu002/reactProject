c3428fcefd0038e5b822ac6d1b15370bc613992f
Tiff <tiffx25+github@gmail.com>
2024-01-08 00:55:24 +0800

    added filter

---
+++ src/components/Filter.tsx
@@ -0,0 +1,59 @@
+import { Box, TextField } from "@mui/material";
+import React, { Dispatch, SetStateAction, useState } from "react";
+
+type FilterProp = {
+  filterValues: any;
+  setFilterValues: Dispatch<SetStateAction<any>>;
+};
+
+export const Filter: React.FC<FilterProp> = ({
+  filterValues,
+  setFilterValues,
+}) => {
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFilterValues({
+      ...filterValues,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      <Box
+        display="flex"
+        flexDirection="column"
+        sx={{ width: "25vw", margin: "4vh 7vw" }}
+      >
+        <p style={{ fontSize: "1.2rem" }}>Filter by:</p>
+        <TextField
+          id="author-filter"
+          label="Author"
+          variant="outlined"
+          name="author"
+          value={filterValues.author}
+          onChange={handleFilterChange}
+          sx={{ margin: "2vh 0" }}
+        />
+        <TextField
+          id="reading-status-filter"
+          label="Reading status"
+          variant="outlined"
+          name="readingStatus"
+          value={filterValues.readingStatus}
+          onChange={handleFilterChange}
+          sx={{ marginBottom: "2vh" }}
+        />
+        <TextField
+          id="title-filter"
+          label="Title"
+          variant="outlined"
+          name="title"
+          value={filterValues.title}
+          onChange={handleFilterChange}
+        />
+      </Box>
+      <hr className="dividerStyle" />
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,18 @@ import { PageHeader } from "../components/Header";
 import { RootState } from "../store";
 import { useEffect, useState } from "react";
 import { CollectionBookCard } from "../components/CollectionBookCard";
+import { Filter } from "../components/Filter";
 
 export const Home = () => {
   const bookCollection = useSelector(
     (state: RootState) => state.bookCollection.myBooks
   );
   const [myBooks, setMyBooks] = useState<any[]>([]);
+  const [filterValues, setFilterValues] = useState({
+    title: "",
+    author: "",
+    readingStatus: "",
+  });
 
   useEffect(() => {
     const storedBooks = localStorage.getItem("bookCollection");
@@ -34,16 +40,30 @@ export const Home = () => {
         }
         return { ...book, status: readingStatus };
       });
-      setMyBooks(booksWithStatus);
-    }
-  }, [bookCollection]);
 
-  // add filter component
-  // get title, author and rating given from filter component
+      console.log(filterValues);
+
+      //filter items
+      const filteredResult = booksWithStatus.filter(
+        (book) =>
+          book.title.toLowerCase().includes(filterValues.title.toLowerCase()) &&
+          book.author_name
+            .join(", ")
+            .toLowerCase()
+            .includes(filterValues.author.toLowerCase()) &&
+          book.status
+            .toLowerCase()
+            .includes(filterValues.readingStatus.toLowerCase())
+      );
+      console.log(filteredResult);
+      setMyBooks(filteredResult);
+    }
+  }, [bookCollection, filterValues]);
 
   return (
     <div>
       <PageHeader pageName="My Collection" />
+      <Filter filterValues={filterValues} setFilterValues={setFilterValues} />
       <section className="myCollectionSection">
         {myBooks.map((mybook) => {
           return <CollectionBookCard key={mybook.key} book={mybook} />;
